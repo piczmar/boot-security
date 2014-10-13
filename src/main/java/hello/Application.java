580e2bb180538b38d88ac6d246783faa27c36185
Marcin Piczkowski <marcin.piczkowski@cybercom.com>
2014-10-13 17:04:06 +0200

    spring security + monitoring, not working JMX connection with jconsole or jvisualvm

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,6 +2,11 @@ package hello;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.Context;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +18,29 @@ public class Application {
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
     }
+//
+//    @Bean
+//    public TomcatEmbeddedServletContainerFactory tomcatFactory() {
+//        return new TomcatEmbeddedServletContainerFactory() {
+//
+//            @Override
+//            protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(
+//                    Tomcat tomcat) {
+//                tomcat.enableNaming();
+//                return super.getTomcatEmbeddedServletContainer(tomcat);
+//            }
+//
+//            @Override
+//            protected void postProcessContext(Context context) {
+////                ContextResource resource = new ContextResource();
+////                resource.setName("jdbc/myDataSource");
+////                resource.setType(DataSource.class.getName());
+////                resource.setProperty("driverClassName", "your.db.Driver");
+////                resource.setProperty("url", "jdbc:yourDb");
+////
+////                context.getNamingResources().addResource(resource);
+//            }
+//        };
+//    }
 
 }
---
+++ src/main/java/hello/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package hello;
+// http://spring.io/blog/2013/07/03/spring-security-java-config-preview-web-security/
+// http://justinrodenbostel.com/2014/05/30/part-5-integrating-spring-security-with-spring-boot-web/
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home")
+                .permitAll()
+                .anyRequest().authenticated();
+        http
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Configuration
+    protected static class AuthenticationConfiguration extends
+            GlobalAuthenticationConfigurerAdapter {
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
+        }
+
+    }
+
+}
